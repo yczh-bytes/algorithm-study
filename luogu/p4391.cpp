f4309300265f4d8db04cf185106bc695343a6b2a
yczh-bytes <3052259152@qq.com>
2026-03-12 21:01:42 +0800

    1

--- luogu/p4391.cpp
+++ luogu/p4391.cpp
@@ -1,2 +1,27 @@
 #include <bits/stdc++.h>
-using namesoace std;
+using namespace std;
+
+char c[1000010];
+int ne[1000010];
+int main()
+{
+    int n;
+    string s;
+    cin>>n;
+    cin>>c+1;
+    for(int i=2,j=0;i<=n;i++)
+    {
+        while(j&&c[i]!=c[j+1])j=ne[j];
+
+        if(c[i]==c[j+1])
+        {
+            j++;
+        }
+
+        ne[i]=j;
+    }
+    
+    int ans = n-ne[n];
+    cout<<ans;
+    return 0;
+}
